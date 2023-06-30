43ed94ab8784a5dc9be51ccf64cfe88f43066076
vicma <vmaldaraca@gmail.com>
2023-06-29 21:09:16 -0600

    Create login, home and signUp initial component

---
+++ src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import SignUp from '../SignUp/signUp';
+import Login from '../Login/Login';
+
+function Home() {
+  return (
+    <main>
+      <SignUp />
+      <Login />
+    </main>
+  );
+}
+
+export default Home;
---
+++ src/components/SignUp/signUp.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SignUp() {
+  return (
+    <div>
+      <h1>Hello from signUp</h1>
+    </div>
+  );
+}
+
+export default SignUp;
