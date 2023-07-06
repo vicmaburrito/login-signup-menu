8172971739461062fec7fbcede9085210dee33b2
vicma <vmaldaraca@gmail.com>
2023-07-06 00:27:15 -0600

    Add style and fields in login component

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,7 +2,20 @@ import React from 'react';
 
 function Login() {
   return (
-    <h1>Hello from Login</h1>
+    <div className="mt-5 d-flex align-items-center">
+      <div className="container px-5">
+        <h2 className="text-center mb-5 outfit">Login</h2>
+        <form>
+          <div className="form-group">
+            <input type="text" className="form-control mb-4 outfit" id="name" placeholder="Name" />
+          </div>
+          <div className="form-group">
+            <input type="email" className="form-control mb-4 outfit" id="email" placeholder="Email" />
+          </div>
+          <button type="submit" className="btn btn-primary">Submit</button>
+        </form>
+      </div>
+    </div>
   );
 }
 
