6ac49f72a62cd0b5b728b6cdacfb9f585d0f8492
vicma <vmaldaraca@gmail.com>
2023-07-05 23:37:26 -0600

    Add styles and bg using vanta in firstblock

--- src/components/FirstBlock/FirstBlock.js
+++ src/components/FirstBlock/FirstBlock.js
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import FOG from 'vanta/dist/vanta.fog.min';
+import * as THREE from 'three';
+import './FirstBlock.css';
 
-export default function FirstBlock() {
+function FirstBlock() {
+  const [vantaEffect, setVantaEffect] = useState(0);
+  const myRef = useRef(null);
+  useEffect(() => {
+    if (!vantaEffect) {
+      setVantaEffect(
+        FOG({
+          el: myRef.current,
+          THREE,
+          mouseControls: true,
+          touchControls: true,
+          gyroControls: false,
+          minHeight: 200.00,
+          minWidth: 200.00,
+          highlightColor: 0xf7f7f7,
+          midtoneColor: 0x9dc567,
+          lowlightColor: 0xfffff,
+          baseColor: 0x064635,
+          blurFactor: 0.28,
+          speed: 2.90,
+          zoom: 1.80,
+        }),
+      );
+    }
+    return () => {
+      if (vantaEffect) vantaEffect.destroy();
+    };
+  }, [vantaEffect]);
   return (
-    <div>
-      <h1>Hello from firstBlock</h1>
-    </div>
+    <section className="masthead d-flex align-items-center" ref={myRef}>
+      <div className="container px-4 px-lg-5 text-center scale-down-center">
+        <h1 className="mb-1 display-4 text-white">Auth Application</h1>
+        <h3 className="mb-5 text-white"><em>by Manuel Aldaraca</em></h3>
+      </div>
+    </section>
   );
 }
+
+export default FirstBlock;
