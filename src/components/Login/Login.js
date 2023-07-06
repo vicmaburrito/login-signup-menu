a35de5f64b22b5fb1f687e00d32931f189cd33c2
vicma <vmaldaraca@gmail.com>
2023-07-06 01:06:06 -0600

    Add icons in login component and install bootstrap icons

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 function Login() {
   return (
-    <div className="mt-5 d-flex align-items-center">
-      <div className="container px-5">
+    <div className="d-flex align-items-center">
+      <div className="container p-5">
         <h2 className="text-center mb-5 outfit">Login</h2>
         <form>
           <div className="form-group">
@@ -12,7 +12,18 @@ function Login() {
           <div className="form-group">
             <input type="email" className="form-control mb-4 outfit" id="email" placeholder="Email" />
           </div>
-          <button type="submit" className="btn btn-primary">Submit</button>
+          <div className="text-center mb-3">
+            <button type="submit" className="btn btn-primary px-5">Submit</button>
+          </div>
+          <div className="text-center">
+            <p className="text-muted">or sign up with:</p>
+            <button type="button" className="btn btn-secondary mx-1">
+              <i className="bi bi-facebook" />
+            </button>
+            <button type="button" className="btn btn-secondary mx-1">
+              <i className="bi bi-google" />
+            </button>
+          </div>
         </form>
       </div>
     </div>
